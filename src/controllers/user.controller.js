255ce2fb2cc6f6c64e392c2d15c56c1b68b11d42
Farooq Haider Alvi <farooqhaideralvi1999@gmail.com>
2024-06-19 22:47:02 +0500

    user registration completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,59 @@
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Salam Pakistan",
+  const { username, fullName, password, email } = req.body;
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const userExist = await User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (userExist) {
+    throw new ApiError(400, "User with email or username already existed.");
+  }
+  console.log(userExist, "i am user", req.files);
+
+  const avatarPath = req.files?.avatar[0]?.path;
+  const coverImagePath = req.files?.coverImage[0]?.path;
+
+  if (!avatarPath) {
+    throw new ApiError(400, "avatar field is required.");
+  }
+  const avatar = await uploadOnCloudinary(avatarPath);
+  const coverImage = await uploadOnCloudinary(coverImagePath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required.");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Somethign went wrong...");
+  }
+  console.log("user is created");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, createdUser, "User created Successfully."));
 });
 
 export { registerUser };
